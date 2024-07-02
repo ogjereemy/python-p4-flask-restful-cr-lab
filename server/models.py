1194cc6dce8f35eb3d5ca2ad66fa3381545dc4a9
ogola <ogolajeremy601@gmail.com>
2024-07-02 14:42:18 +0300

    adds cr-lab

--- server/models.py
+++ server/models.py
@@ -7,3 +7,6 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    image = db.Column(db.String(255))
+    price = db.Column(db.Float)
